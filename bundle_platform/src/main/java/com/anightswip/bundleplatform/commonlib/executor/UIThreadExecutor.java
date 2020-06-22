6b45de5e65cf4659d882578f5c2544a3e0d41995
maker1024 <sang_1024@163.com>
2020-06-22 10:14:42 +0800

    新增AppExecutors测试

--- bundle_platform/src/main/java/com/anightswip/bundleplatform/commonlib/executor/UIThreadExecutor.java
+++ bundle_platform/src/main/java/com/anightswip/bundleplatform/commonlib/executor/UIThreadExecutor.java
@@ -12,7 +12,15 @@ import java.util.concurrent.Executor;
  */
 public class UIThreadExecutor implements Executor {
 
-    private Handler uiThreadHandler = new Handler(Looper.getMainLooper());
+    private Handler uiThreadHandler;
+
+    public UIThreadExecutor() {
+        uiThreadHandler = new Handler(Looper.getMainLooper());
+    }
+
+    public UIThreadExecutor(Looper mainLoop) {
+        uiThreadHandler = new Handler(mainLoop);
+    }
 
     @Override
     public void execute(@NonNull Runnable command) {
---
+++ bundle_platform/src/test/java/com/anightswip/bundleplatform/commonlib/executor/AppExecutorsTest.java
@@ -0,0 +1,56 @@
+package com.anightswip.bundleplatform.commonlib.executor;
+
+import android.os.Build;
+
+import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.O_MR1)
+public class AppExecutorsTest {
+
+    @Test
+    public void appExecutorsTest() {
+
+        assertThrows(IllegalStateException.class, new ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                AppExecutors.uiThread();
+            }
+        });
+
+        assertThrows(IllegalStateException.class, new ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                AppExecutors.workThread();
+            }
+        });
+
+        AppExecutors.init(Executors.newSingleThreadExecutor()
+                , new UIThreadExecutor(RuntimeEnvironment.systemContext.getMainLooper()));
+        AppExecutors.workThread().execute(new Runnable() {
+            @Override
+            public void run() {
+                assertFalse(RuntimeEnvironment.isMainThread());
+            }
+        });
+
+        AppExecutors.uiThread().execute(new Runnable() {
+            @Override
+            public void run() {
+                assertTrue(RuntimeEnvironment.isMainThread());
+            }
+        });
+    }
+
+}
